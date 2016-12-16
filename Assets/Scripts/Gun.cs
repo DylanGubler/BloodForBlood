9fed715b850613ac79b5234f3e6f10aaf205cf2c
DylanGubler <Dylan_Gubler@Hotmail.com>
2016-12-16 08:26:54 -0800

    The Game!
    I lost it

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public enum FireMode {Auto, Burst, Single};
+	public FireMode fireMode;
+
+	public Transform muzzle;
+	public Projectile projectile;
+	public float msBetweenShots = 100;
+	public float muzzleVelocity = 35;
+
+	public Transform shell;
+	public Transform shellEjection;
+	MuzzleFlash muzzleflash;
+
+	bool triggerReleasedSinceLastShot;
+
+	void Start() {
+		muzzleflash = GetComponent<MuzzleFlash>();
+	}
+
+	float nextShotTime;
+
+	void Shoot() {
+
+		if (Time.time > nextShotTime) {
+			nextShotTime = Time.time + msBetweenShots / 1000;
+			Projectile newProjectile = Instantiate (projectile, muzzle.position, muzzle.rotation) as Projectile;
+			newProjectile.SetSpeed (muzzleVelocity);
+
+			Instantiate (shell, shellEjection.position, shellEjection.rotation);
+			muzzleflash.Activate();
+		}
+	}
+
+	public void OnTriggerHold() {
+		Shoot ();
+		triggerReleasedSinceLastShot = false;
+	}
+
+	public void OnTriggerRelease() {
+		triggerReleasedSinceLastShot = true;
+	}
+}
